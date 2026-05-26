89635e546e28b5e99d59eaa240beb461b0ceca7c
yPedroksz <sacodeb@gmail.com>
2026-05-26 20:48:12 -0300

    Ladrao.java

---
+++ Ladrao.java
@@ -0,0 +1,5 @@
+package inimigos;
+public class Ladrao {
+    public static final int HP = 85, ATK = 22, XP = 100, OURO = 200;
+    public static final int HP_ALERTA = 90, ATK_ALERTA = 25, OURO_ALERTA = 50;
+}
